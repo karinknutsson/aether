6224293f585ad8298930e2dc2bcee501af455f7d
Karin Knutsson <karinmiriam@gmail.com>
2025-04-03 12:34:53 +0200

    added map

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div ref="mapContainer" id="map" class="map-container"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import mabpoxgl from 'mapbox-gl'
+
+const apiKey = process.env.MAPBOX_API_KEY
+const mapContainer = ref(null)
+const dataMarkers = [{ lng: 13.407557, lat: 52.509237 }]
+
+onMounted(() => {
+  const map = new mabpoxgl.Map({
+    container: 'map',
+    // frank
+    // style: 'mapbox://styles/karinmiriam/cko88xcgl4vju18pgby8zpan6',
+    // aetheric
+    style: 'mapbox://styles/karinmiriam/cm915r01k009b01s457lv403i',
+    zoom: 12,
+    center: [13.407557, 52.509237],
+    accessToken: apiKey ?? '',
+  })
+
+  dataMarkers.forEach((marker) => {
+    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map)
+  })
+
+  // map.addControl(new mapboxgl.NavigationControl())
+})
+</script>
+
+<style scoped lang="scss">
+:deep(a) {
+  color: $font-color;
+  text-decoration: none;
+  font-size: 8px;
+}
+
+.map-container {
+  width: 1000px;
+  height: 600px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,33 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1 class="logo">ÆTHER</h1>
+  <q-page class="row items-center justify-center">
+    <div>
+      <div class="blue-div"></div>
+      <h1 class="logo">ÆTHER</h1>
+      <div class="red-div"></div>
+    </div>
+
+    <div><MapComponent /></div>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MapComponent from 'src/components/MapComponent.vue'
+</script>
+
+<style scoped lang="scss">
+.blue-div {
+  width: 320px;
+  height: 320px;
+  border-radius: 50%;
+  background: $primary;
+  // transform: translateY(-40px);
+}
+
+.red-div {
+  width: 160px;
+  height: 160px;
+  border-radius: 50%;
+  background: $secondary;
+  // transform: translateY(-40px);
+}
+</style>
